461ce441f74d42175fa946a72d6b55a7a2bd0992
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-14 14:09:06 +0300

    Add build & deploy functionality

---
+++ build.clj
@@ -0,0 +1,44 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require
+   [clojure.tools.build.api :as b]
+   [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'com.ovistoica/ring-nexus-middleware)
+(def version "2025.07.14")
+(def class-dir "target/classes")
+
+(defn- jar-opts [opts]
+  (assoc opts
+         :lib lib :version version
+         :jar-file (format "target/%s-%s.jar" lib version)
+         :basis (b/create-basis {})
+         :class-dir class-dir
+         :target "target"
+         :src-dirs ["src"]))
+
+(defn jar
+[opts]
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (println "\nBuilding JAR...")
+    (b/jar opts))
+  opts)
+
+(defn install
+  "Install the JAR locally."
+  [opts]
+  (let [opts (jar-opts opts)]
+    (b/install opts))
+  opts)
+
+(defn deploy "Deploy the JAR to Clojars."
+  [opts]
+  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
+    (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
+  opts)
