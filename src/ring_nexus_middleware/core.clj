eb767196ae109255098d4dd05899f0c95da74948
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-14 11:21:49 +0300

    Initial setup for tests

--- src/ring_nexus_middleware/core.clj
+++ src/ring_nexus_middleware/core.clj
@@ -1,1 +1,16 @@
-(ns ring-nexus-middleware.core)
+(ns ring-nexus-middleware.core
+  (:require [nexus.core :as nexus]))
+
+(defn wrap-nexus
+  [handler nexus-map system]
+  (fn [request]
+    (let [response (atom nil)
+          actions (handler request)]
+      (nexus/dispatch nexus-map system {:request request} actions)
+      ;; This could be done with core async better
+      (loop []
+        (if @response
+          @response
+          (do 
+            (Thread/sleep 50)
+            (recur)))))))
--- test/ring_nexus_middleware/core_test.clj
+++ test/ring_nexus_middleware/core_test.clj
@@ -1,3 +1,13 @@
 (ns ring-nexus-middleware.core-test
-  (:require [clojure.test :refer [deftest]]
-            [ring-nexus-middleware.core :as core]))
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [ring-nexus-middleware.core :as core]))
+
+(deftest wrap-nexus-test
+  (testing "Ring response effects"
+    (let [handler (-> (fn [req] [[:http-response/ok {:message "Success"}]])
+                      (core/wrap-nexus {} {}))]
+      (is (= (handler {})
+             {:status 200
+              :body {:message "Success"}})))
+    ))
