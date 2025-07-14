82f9d2213e3b7098f936df7e44ec6433f39282fd
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-14 12:58:22 +0300

    Add actions for convenience ring responses

--- src/ring_nexus_middleware/core.clj
+++ src/ring_nexus_middleware/core.clj
@@ -5,9 +5,32 @@
   [nexus request respond raise]
   (-> nexus
       (assoc-in [:nexus/effects :http/respond]
-                (fn [_ _ response-map] (respond response-map)))
+                (fn [_ _ {:keys [body status headers], :as response-map}]
+                  (respond (cond-> response-map
+                             (nil? body) (assoc :body "")
+                             (nil? headers) (assoc :headers {})
+                             (nil? status) (assoc :status 200)))))
       (assoc-in [:nexus/actions :http-response/ok]
-                (fn [_ response-body] [[:http/respond {:status 200 :body response-body}]]))))
+                (fn [_ response-body] [[:http/respond
+                                        {:status 200, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/created]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 201, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/bad-request]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 400, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/unauthorized]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 401, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/forbidden]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 403, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/not-found]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 404, :body response-body}]]))
+      (assoc-in [:nexus/actions :http-response/internal-server-error]
+                (fn [_ response-body] [[:http/respond
+                                        {:status 500, :body response-body}]]))))
 
 (defn wrap-nexus
   [handler nexus-map system]
--- test/ring_nexus_middleware/core_test.clj
+++ test/ring_nexus_middleware/core_test.clj
@@ -1,26 +1,53 @@
 (ns ring-nexus-middleware.core-test
-  (:require
-   [clojure.test :refer [deftest is testing]]
-   [ring-nexus-middleware.core :as core]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [ring-nexus-middleware.core :as core]))
 
 (deftest wrap-nexus-test
   (testing "Ring response effect"
-    (let [handler (-> (fn [_] [[:http/respond {:body {:message "Success"}
-                                               :status 200}]])
+    (let [handler (-> (fn [_] [[:http/respond
+                                {:body {:message "Success"}, :status 200}]])
                       (core/wrap-nexus {:nexus/system->state identity} {}))
           response (atom nil)
           respond #(reset! response %)]
       (handler {} respond identity)
       (is (= @response
-             {:status 200
-              :body {:message "Success"}}))))
-
+             {:status 200, :headers {}, :body {:message "Success"}}))))
   (testing "Ring response convenience actions"
-    (let [handler (-> (fn [_] [[:http-response/ok {:message "Success"}]])
+    (let [handler (-> [req]
+                      (fn
+                        (let [{:keys [action action-body]} (:body req)]
+                          [[action action-body]]))
                       (core/wrap-nexus {:nexus/system->state identity} {}))
-          response (atom nil)
-          respond #(reset! response %)]
-      (handler {} respond identity )
-      (is (= @response
-             {:status 200
-              :body {:message "Success"}})))))
+          responses (atom [])
+          respond #(swap! responses conj %)]
+      (handler {:body {:action :http-response/ok, :action-body {:message "ok"}}}
+               respond
+               identity)
+      (handler {:body {:action :http-response/bad-request,
+                       :action-body {:message "bad-request"}}}
+               respond
+               identity)
+      (handler {:body {:action :http-response/unauthorized,
+                       :action-body {:message "unauthorized"}}}
+               respond
+               identity)
+      (handler {:body {:action :http-response/not-found,
+                       :action-body {:message "not-found"}}}
+               respond
+               identity)
+      (handler {:body {:action :http-response/internal-server-error,
+                       :action-body {:message "internal-server-error"}}}
+               respond
+               identity)
+      (handler {:body {:action :http-response/forbidden,
+                       :action-body {:message "forbidden"}}}
+               respond
+               identity)
+      (is
+        (= @responses
+           [{:body {:message "ok"}, :headers {}, :status 200}
+            {:body {:message "bad-request"}, :headers {}, :status 400}
+            {:body {:message "unauthorized"}, :headers {}, :status 401}
+            {:body {:message "not-found"}, :headers {}, :status 404}
+            {:body {:message "internal-server-error"}, :headers {}, :status 500}
+            {:body {:message "forbidden"}, :headers {}, :status 403}])))))
